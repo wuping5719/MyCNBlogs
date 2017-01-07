5e30f854480eb3d9e0a6e4c00110ab833d2409dc
PingWu <wp571988@163.com>
2017-01-07 16:06:48 +0800

    Create jq.snow.js

---
+++ CNBlogs-SourceCode/js/jq.snow.js
@@ -0,0 +1,42 @@
+
+(function($){
+
+	$.fn.snow = function(options){
+
+			var $flake 			= $('<div id="snowbox" />').css({'position': 'absolute', 'top': '-50px'}).html('&#10052;'),
+				documentHeight 	= $(document).height(),
+				documentWidth	= $(document).width(),
+				defaults		= {
+									minSize		: 10,		
+									maxSize		: 20,		
+									newOn		: 1000,		
+									flakeColor	: "#FFFFFF"
+								},
+				options			= $.extend({}, defaults, options);
+
+			var interval		= setInterval( function(){
+				var startPositionLeft 	= Math.random() * documentWidth - 100,
+				 	startOpacity		= 0.5 + Math.random(),
+					sizeFlake			= options.minSize + Math.random() * options.maxSize,
+					endPositionTop		= documentHeight - 40,
+					endPositionLeft		= startPositionLeft - 100 + Math.random() * 500,
+					durationFall		= documentHeight * 10 + Math.random() * 5000;
+				$flake.clone().appendTo('body').css({
+							left: startPositionLeft,
+							opacity: startOpacity,
+							'font-size': sizeFlake,
+							color: options.flakeColor
+						}).animate({
+							top: endPositionTop,
+							left: endPositionLeft,
+							opacity: 0.2
+						},durationFall,'linear',function(){
+							$(this).remove()
+						}
+					);
+
+			}, options.newOn);
+
+	};
+
+})(jQuery);
